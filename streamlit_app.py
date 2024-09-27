a78e1b707db95b014ad35b6ff8e04e660fb2125d
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-27 18:30:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 from sklearn.cluster import KMeans
 import sklearn
-pip install scikit-learn
+! pip install scikit-learn
 st.title("KMeans Model Predictor")
 st.image("Customer-Segmentation.png", caption="Customer-Segmentation", use_column_width=True)
 
