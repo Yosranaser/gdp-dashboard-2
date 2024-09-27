8d777573f4c8b3d4ddb1a3452bd73a249f4a9168
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-27 22:37:43 +0300

    Create DASHBOARD.py

---
+++ pages1/DASHBOARD.py
@@ -0,0 +1,36 @@
+
+import streamlit as st
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import rarfile
+import os
+
+
+# Load the KMeans model
+with open('kmeans_model (1).pkl', 'rb') as file:
+    kmeans_model = pickle.load(file)
+df = pd.read_csv('cleaned_df.csv')
+
+
+# Path to the .rar file
+#rar_path = 'data.rar'
+
+# Extract the .rar file
+#with rarfile.RarFile(rar_path) as rf:
+    #rf.extractall()  # Extracts files to the current directory
+
+# Assuming the extracted file is named 'data.csv'
+#csv_file_path = 'data.csv'
+
+# Check if the file was extracted and exists
+#if os.path.exists(csv_file_path):
+    #df = pd.read_csv(csv_file_path)
+    #st.write(df)  # Display the dataframe
+#else:
+    #st.write("Extracted file not found.")
+ 
+
+# Streamlit layout with three columns
+st.title("Customer Segmentation Dashboard")
